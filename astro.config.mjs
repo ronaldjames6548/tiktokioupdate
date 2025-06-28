19b403b447a69f1d011e4f90220437fa29ec9077
pakvedios <pakvedi.os@gmail.com>
2025-06-28 18:40:12 +0500

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -65,14 +65,48 @@ export default defineConfig({
     }),
     icon(),
     solidJs(),
-	sitemap({
+sitemap({
   filter(page) {
+    const excludedPages = [
+		'/ar/blog/',
+		'/ar/blog/best-entertainment-apps-that-must-be-in-your-bookmark-list/',
+		'/ar/blog/best-online-earning-games-in-pakistan-that-are-trending-on-tiktok/',
+		'/ar/blog/buy-a-tiktok-account-a-smart-shortcut-for-fast-growth/',
+		'/ar/blog/capcut-mod-apk-download-v13-7-0-premium-unlocked-2025/',
+		'/ar/blog/comprehensive-anime-streaming-with-gogo-anime-for-android/',
+		'/ar/blog/download-slideshare-ppt-slides-slideshare-pdf-presentations-without-login/',
+		'/ar/blog/harnessing-the-power-of-tiktok/',
+		'/ar/blog/how-to-download-all-of-your-tiktok-videos-with-online-tiktok-downloader/',
+		'/ar/blog/how-to-download-tiktok-videos-on-iphone-android-and-pc-tiktokio/',
+		'/ar/blog/how-to-save-tiktok-videos-without-watermark/',
+		'/ar/blog/how-to-see-top-artists-on-spotify-music-app/',
+		'/ar/blog/iganony-instagram-anonymous-story-viewer-review-how-to-use-it/',
+		'/ar/blog/introducing-decap-cms-your-go-to-solution-for-content-management/',
+		'/ar/blog/introduction-to-astro-a-new-front-end-framework/',
+		'/ar/blog/kk222-game-download-apk-real-money-app/',
+		'/ar/blog/manok-na-pula-the-best-multiplayer-cockfighting-game-2025/',
+		'/ar/blog/royal-x-casino-apk-real-earning-app-download-for-android/',
+		'/ar/blog/royal-x-casino-download-official-app-v-2-30-6-latest-version/',
+		'/ar/blog/simplifying-front-end-interactivity-with-alpine-js/',
+		'/ar/blog/spike-mod-apk-2025-v5-9-205-unlocked-all-characters-max-level/',
+		'/ar/blog/the-power-and-potential-of-chatgpt/',
+		'/ar/blog/the-power-of-typography-in-the-digital-era/',
+		'/ar/blog/unleashing-the-power-of-tailwind-css/',
+		'/ar/blog/using-tiktok-for-more-than-entertainment-how-digital-platforms-empower-users-with-legal-knowledge/',
+		'/ar/blog/what-is-jenny-mod-a-complete-guide-to-the-mod-and-its-characters/',
+		'/ar/blog/what-is-minecraft-how-to-become-a-pro-player/',
+		'/ar/blog/what-is-tiktok-18-tips-to-get-viral/',
+	];
+
     const exclude =
+      // Exclude tag and category pages
       page.includes('/tag/') ||
       page.includes('/category/') ||
-      /\/blog\/\d+/.test(page) ||
-      // Only exclude translated blog posts, not main English blog or other pages
-      /^\/(ar|de|es|fr|hi|id|it|ko|ms|nl|pt|ru|tl|tr)\/blog\//.test(page);
+
+      // Exclude paginated blog routes like /blog/2/
+      /\/blog\/\d+/.test(page) 
+      // Exclude manually listed pages
+      excludedPages.includes(page);
 
     return !exclude;
   }
