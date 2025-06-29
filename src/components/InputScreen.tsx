07f45757eb57986d1e80f49005bee1d42f2e154b
pakvedios <pakvedi.os@gmail.com>
2025-06-29 09:15:55 +0500

    commit form

--- src/components/InputScreen.tsx
+++ src/components/InputScreen.tsx
@@ -81,7 +81,7 @@ function InputScreen({}: Props) {
 {/* input data start */}
 <div class="max-w-6xl mx-auto">
     <div class="download-box p-1 rounded-2xl shadow-xl">
-        <div class="backdrop-blur-md rounded-xl">
+        <div class="bg-gray-800/80 rounded-xl backdrop-blur-md p-6">
             <form class="flex flex-col md:flex-row items-stretch md:items-center gap-2"
               onSubmit={(e) => {
               e.preventDefault();
