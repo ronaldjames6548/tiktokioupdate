831742d6cde9b440727b56aaadd631e8b3b20ec1
pakvedios <pakvedi.os@gmail.com>
2025-07-02 22:39:09 +0500

    commit

--- src/utils/localize.ts
+++ src/utils/localize.ts
@@ -1,14 +1,12 @@
 // src/utils/localize.ts
-import { Astro } from "astro";
 
-export const locales = ['en', 'it', 'fr', 'de', 'es', 'hi', 'ar', 'id', 'ru', 'pt', 'ko', 'tl', 'nl', 'ms', 'tr']; // Add all your supported locales
-
+export const locales = ['en', 'it', 'fr', 'de', 'es', 'hi', 'ar', 'id', 'ru', 'pt', 'ko', 'tl', 'nl', 'ms', 'tr'];
 
 export function getUrl(locale: string, path: string = ''): string {
   const segments = path.split('/').filter(Boolean);
 
   // Replace existing locale prefix if present
-  if (['en', 'de'].includes(segments[0] || '')) {
+  if (segments[0] && locales.includes(segments[0])) {
     segments[0] = locale;
   } else {
     segments.unshift(locale);
