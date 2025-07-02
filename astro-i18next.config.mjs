ad37dfa89c880a0f865eae0247b446fef939a632
pakvedios <pakvedi.os@gmail.com>
2025-07-02 21:53:59 +0500

    commit

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -8,7 +8,7 @@ export default {
   defaultLangHasNoSuffix: true,
 
   // Set base URL for hreflang canonical URLs
-  baseUrl: "https://tiktokioupdate.vercel.app ",
+  baseUrl: "https://tiktokioupdate.vercel.app",
 
   // Map locales to their route overrides (optional)
   routeOverrides: {
